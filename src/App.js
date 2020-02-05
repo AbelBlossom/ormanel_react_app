3d5e90b53ff278f3b2ee764bb29d39706347bff6
abel <abeldzan@gmail.com>
2020-02-05 10:58:13 +0000

    first comit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+// @flow
+/* eslint-disable react/jsx-filename-extension */
+import React from 'react'
+
+function App() {
+  return (
+    <div>
+      <h1>My react template</h1>
+    </div>
+  )
+}
+
+export default App
